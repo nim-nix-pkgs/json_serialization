197823d563f8393e6566a169f3d0d36a14eb5153
hugosenari <hugosenari@gmail.com>
2022-03-12 01:53:44 -0300

    chore: re index json_serialization

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Flexible JSON serialization not relying on run-time type information'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."json_serialization-master".url = "path:./master";
+  inputs."json_serialization-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."json_serialization-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
