5f73677669dcd2ff0cad59d8501a513544a07bb5
hugosenari <hugosenari@gmail.com>
2022-03-27 21:58:26 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-json_serialization-master.ref   = "refs/heads/master";
   inputs.src-json_serialization-master.owner = "status-im";
   inputs.src-json_serialization-master.repo  = "nim-json-serialization";
-  inputs.src-json_serialization-master.dir   = "";
   inputs.src-json_serialization-master.type  = "github";
   
   inputs."serialization".owner = "nim-nix-pkgs";
